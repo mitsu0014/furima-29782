c9434b147f7804ebbd8394150e3acabeeabfe2ea
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-09-09 18:44:46 +0900

    user_signed_in? 出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,17 @@
 class ItemsController < ApplicationController
-
+  before_action :move_to_index, except: [:index]
   # before_action :authenticate_user!
     
   def index
   end
 
   def new
+  end
+
+  private
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
