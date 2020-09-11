d61e3ba74bcb40e948d25f19b9dcd2d2c9d3edd3
mitsu0014 <70260498+mitsu0014@users.noreply.github.com>
2020-09-11 10:53:07 +0900

    プルリクエスト訂正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -34,10 +34,16 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Price can't be blank")
     end
 
-    it '販売価格が¥300~¥9,999,999の間' do
+    it '販売価格が¥300以上でないとだめ' do
       @item.price = '299'
       @item.valid?
       expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+    end
+
+    it '販売価格が¥9999999以下でないとだめ' do
+      @item.price = '10000000'
+      @item.valid?
+      expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
     end
 
     it '販売価格が半角数字でないとだめ' do
